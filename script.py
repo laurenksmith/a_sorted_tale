f793a254ed98f53435cbffbbe5f193159da40ce2
Lauren Smith <laurenkimsmith@gmail.com>
2025-05-01 12:37:14 +0100

    I have now amended my code in the script.py file to print the lowercase titles.

--- script.py
+++ script.py
@@ -4,8 +4,5 @@ import sorts
 bookshelf = utils.load_books('books_small.csv')
 
 # add a for loop that will print the titles within the bookshelf of small books
-book: str
 for book in bookshelf:
     print(book['title_lower'])
-
-
