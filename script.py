191e2293a1df026fe31487e8567dff4bbd400c9f
Lauren Smith <laurenkimsmith@gmail.com>
2025-05-01 12:57:16 +0100

    Added a sort comparison function to my script file

--- script.py
+++ script.py
@@ -6,3 +6,9 @@ bookshelf = utils.load_books('books_small.csv')
 # add a for loop that will print the titles within the bookshelf of small books
 for book in bookshelf:
     print(book['title_lower'])
+
+
+# add a sort comparison function
+def by_title_ascending(book_a, book_b):
+    return book_a['title_lower'] > book_b['title_lower']
+
